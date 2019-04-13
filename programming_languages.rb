c958fe11a7543d78e389ce4a10cad4e46399f74b
Conor Owens <conorj.owens@gmail.com>
2019-04-13 19:42:21 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,6 +1,9 @@
+require "pry"
+
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, hash|
+    binding.pry
     hash.each do |language, hash2|
       new_hash[language] = hash2
     end
