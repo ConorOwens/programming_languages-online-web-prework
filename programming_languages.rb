5333417b34fad9bbe89292160c52fe2af2c30d82
Conor Owens <conorj.owens@gmail.com>
2019-04-13 20:06:29 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,14 +6,16 @@ def reformat_languages(languages)
    # binding.pry
     hash.each do |language, hash2|
       #binding.pry
-      if new_hash[language] = hash2
+      if new_hash[language]
       #binding.pry
-      if new_hash[language][:style] == nil
+        if new_hash[language][:style]
+          new_hash[language][:style] << style
         #binding.pry
-        new_hash[language][:style] = [style]
-      else
-        binding.pry
-        new_hash[language][:style] << style
+        else
+        #binding.pry
+          new_hash[language][:style] = [style]
+        end
+      else new_hash[language] = hash2
       end
     end
   end
